8a0e1da300788bd2f7ebee904f3ee3ad81667d51
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-09 16:00:54 -0600

    Added: new route for users

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import { corsMiddleware } from "./middleware/cors.js";
 import { glassesRouter } from "./routes/glasses.js";
+import { userRouter } from "./routes/user.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
@@ -8,6 +9,7 @@ const PORT = process.env.PORT ?? 5000;
 app.use(json());
 app.use(corsMiddleware());
 app.use("/glasses", glassesRouter);
+app.use("/users", userRouter);
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
