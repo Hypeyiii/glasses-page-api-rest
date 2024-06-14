0969e0768c0180213fc5e2ed24583a112e050946
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-14 05:41:42 -0600

    added: new routes for users and checkout, and new middleware for cookies-parser

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ import express, { json } from "express";
 import { corsMiddleware } from "./middleware/cors.js";
 import { glassesRouter } from "./routes/glasses.js";
 import { userRouter } from "./routes/user.js";
+import { stripeRouter } from "./routes/stripe.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT ?? 5000;
 
 app.use(json());
+app.use(cookieParser());
 app.use(corsMiddleware());
+
+app.use("/checkout", stripeRouter);
 app.use("/glasses", glassesRouter);
 app.use("/users", userRouter);
+
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
