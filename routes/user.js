8d6340aa346c83771097778dd502f0398b5c6d03
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-14 05:50:05 -0600

    🆕 Add users routes for CRUD operations

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
-
 import { UserController } from "../controllers/usersController.js";
 
 export const userRouter = Router();
 
-userRouter.get("/", UserController.getAll);
-userRouter.get("/:id", UserController.getById);
+userRouter.get("/verify", UserController.verify);
+userRouter.get("/logout", UserController.logout);
+
+
 userRouter.get("/email/:email", UserController.getByEmail);
-userRouter.post("/", UserController.create);
+userRouter.get("/:id", UserController.getById);
 userRouter.delete("/:id", UserController.delete);
+userRouter.get("/", UserController.getAll);
+userRouter.post("/register", UserController.register);
+userRouter.post("/login", UserController.login);
