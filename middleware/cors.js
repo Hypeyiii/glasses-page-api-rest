e408ea5a68841b0b09c0fd851fc2acab6f7357a5
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-14 05:48:17 -0600

    ⬆️ Update cors middleware for cookies-parser, CRUD operations and JWT

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,24 +1,20 @@
-// MIDDLEWARE PARA SABER QUE ORIGENES SON PERMITIDOS PARA ACCEDER A LA API
-
 import cors from "cors";
 
 const ACCEPTED_ORIGINS = [
   "http://localhost:8080",
   "http://localhost:5173",
-  "https://isaac-glasses-page.vercel.app/",
+  "https://isaac-glasses-page.vercel.app",
 ];
 
 export const corsMiddleware = ({ accepted_origins = ACCEPTED_ORIGINS } = {}) =>
   cors({
     origin: (origin, callback) => {
-      if (accepted_origins.includes(origin)) {
+      if (accepted_origins.includes(origin) || !origin) {
         return callback(null, true);
       }
-
-      if (!origin) {
-        return callback(null, true);
-      }
-
       callback(new Error("No permitido por CORS"));
     },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   });
