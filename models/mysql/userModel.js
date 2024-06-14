ecf8dc1ddace314969e960cb92fcff4239ea395e
Hypeyiii <isaa.friaslns@uanl.edu.mx>
2024-06-14 05:49:31 -0600

    🆕 Add new user model following the CRUD operations with MySQL

--- models/mysql/userModel.js
+++ models/mysql/userModel.js
@@ -2,6 +2,7 @@
 import { connection } from "../../db.js";
 import bcrypt from "bcrypt";
 import { randomUUID } from "crypto";
+import { SALT_ROUNDS } from "../../config.js";
 
 export class UserModel {
   static async getAll({ search }) {
@@ -30,18 +31,35 @@ export class UserModel {
       [email]
     );
     if (user.length === 0) return null;
-    return user[0];
+    const { password, ...rest } = user[0];
+    return rest;
   }
-
-  static async create({ input }) {
-    const { email, password, username, role } = input;
+  static async register({ input }) {
+    const { email, password, username } = input;
     const id = randomUUID();
+    const role = "client";
 
     try {
+      // Verificación de campos vacíos
+      if (!email) {
+        throw new Error("El email no puede estar vacío");
+      }
+      if (!password) {
+        throw new Error("La contraseña no puede estar vacía");
+      }
+      if (!username) {
+        throw new Error("El nombre de usuario no puede estar vacío");
+      }
+
+      // Verificación de existencia de email y username
       const [existingEmail] = await connection.query(
         "SELECT id FROM Users WHERE email = ?",
         [email]
       );
+
+      if (existingEmail.length > 0) {
+        throw new Error("El correo electrónico ya está en uso");
+      }
 
       const [existingUsername] = await connection.query(
         "SELECT id FROM Users WHERE username = ?",
@@ -52,27 +70,43 @@ export class UserModel {
         throw new Error("El nombre de usuario ya está en uso");
       }
 
-      if (existingEmail.length > 0) {
-        throw new Error("El correo electrónico ya está en uso");
-      }
+      // Hasheo de la contraseña
+      const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      if (!email) {
-        throw new Error("El email no puede estar vacío");
-      }
-      if (!password) {
-        throw new Error("La contraseña no puede estar vacía");
-      }
-
+      // Inserción del nuevo usuario en la base de datos
       await connection.query(
         "INSERT INTO Users (id, email, password, username, role) VALUES (?, ?, ?, ?, ?)",
         [id, email, hashedPassword, username, role]
       );
     } catch (e) {
       console.error("Error al crear el usuario", e);
-      throw e;
+      throw new Error("Error al crear el usuario: " + e.message);
     }
+  }
+
+  static async login({ email, password }) {
+    const [user] = await connection.query(
+      "SELECT * FROM Users WHERE email = ?",
+      [email]
+    );
+
+    if (!user) {
+      throw new Error("Este correo no está registrado");
+    }
+
+    if (user.length === 0) {
+      throw new Error("Ingrese su contraseña");
+    }
+
+    const userPassword = user[0].password;
+
+    const passwordMatches = await bcrypt.compare(password, userPassword);
+
+    if (!passwordMatches) {
+      throw new Error("El correo electrónico o contraseña no coindicen");
+    }
+
+    return user[0];
   }
 
   static async delete({ id }) {
@@ -83,4 +117,8 @@ export class UserModel {
       throw e;
     }
   }
+
+  static async logout() {}
+
+  static async verify() {}
 }
